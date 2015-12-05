9372543d5ebce7978b698d98065d4d998d5a51a7
rooid <vsinhsawant@gmail.com>
2015-12-04 22:40:43 -0500

    demo

---
+++ demo/test_me.js
@@ -0,0 +1,15 @@
+
+var https = require('https');
+var fs = require('fs');
+
+console.log('running 9000' )
+
+var options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+var a = https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  res.end("hello world\n");
+}).listen(9100);
